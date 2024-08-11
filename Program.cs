7f1578d0ab2c4f3ad38636f10d0f49be68b2e5f4
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2024-08-12 00:49:37 +0300

    Prefer index.html to HomeController

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,8 +11,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-//    app.UseExceptionHandler("/Home/Error");
+    //    app.UseExceptionHandler("/Home/Error");
 }
+
+// Set default file to index.html
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    DefaultFileNames = new List<string> { "index.html" }
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
