17b4b8ed2065e7b8e41874c482dd59633942cf37
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2024-08-12 14:05:10 +0300

    Compacting code

--- Program.cs
+++ Program.cs
@@ -30,32 +30,19 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
-
-// Map /patient-monitoring to serve the index.html file
-app.MapGet("/patient-monitoring", async context =>
-{
-    context.Response.ContentType = "text/html";
-    await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, "patient-monitoring", "index.html"));
-});
-
-// Map /patient-monitoring to serve the index.html file
-app.MapGet("/addPatient", async context =>
-{
-    context.Response.ContentType = "text/html";
-    await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, "addPatient", "index.html"));
-});
-
-for (int i = 0; i < 1000; i++)
-{
-    var path = "builder;patientId=" + i;
-    // Map /patient-monitoring to serve the index.html file
-    app.MapGet("/"+path, async context =>
+Action<string> mapToIndexHTML = (s) => {
+    app.MapGet("/"+s, async context =>
     {
         context.Response.ContentType = "text/html";
-        await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, path, "index.html"));
+        await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, s, "index.html"));
     });
-}
+};
+
+// Map urls to serve the index.html file
+mapToIndexHTML("patient-monitoring");
+mapToIndexHTML("addPatient");
+for (int i = 0; i < 1000; i++)
+    mapToIndexHTML("builder;patientId=" + i);
 
 
 //Start server
