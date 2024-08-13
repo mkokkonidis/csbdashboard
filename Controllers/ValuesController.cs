13e145eeb7afc7902f30c71afc3dd9a825d0a2f3
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2024-08-13 13:15:02 +0300

    Temporary change for testing

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace CSBDashboardServer.Controllers
     public class ValuesController : ControllerBase
     {
         const bool Verbose = false;
-        static readonly long epochTicks = new DateTime(1970, 1, 1, 0, 0, 0, 0).Ticks;
+        static readonly long epochTicks = new DateTime(1971, 1, 1, 0, 0, 0, 0).Ticks;
         
         static dynamic CompactObservations(string auth, string apiBaseUrlSlash, int patient,string spec)
         {
