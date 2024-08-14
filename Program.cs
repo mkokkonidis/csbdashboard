f0ba19dcfda1d1adee86e9d7640738894b0f7e27
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2024-08-14 13:06:03 +0300

    Map all non-file URLs to index.html

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,24 +27,37 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-Action<string> mapToIndexHTML = (path) =>
+app.Use(async (context, next) =>
 {
-    app.MapGet(path, async context =>
-    {
-        context.Response.ContentType = "text/html";
-        await context.Response.SendFileAsync("wwwroot/index.html");
-    });
-};
+    var path = context.Request.Path.Value;
 
-//Static mapping
-mapToIndexHTML("/patients-monitoring");
-mapToIndexHTML("/addPatient");
-for (int i = 0; i < 1000; i++)
-    mapToIndexHTML("/builder;patientId=" + i);
+    if (!File.Exists(Path.Combine(app.Environment.WebRootPath, path.TrimStart('/'))))
+    {
+        context.Request.Path = "/index.html";
+    }
+    await next();
+});
+app.MapControllers();
+
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Action<string> mapToIndexHTML = (path) =>
+//{
+//    app.MapGet(path, async context =>
+//    {
+//        context.Response.ContentType = "text/html";
+//        await context.Response.SendFileAsync("wwwroot/index.html");
+//    });
+//};
+
+////Static mapping
+//mapToIndexHTML("/patients-monitoring");
+//mapToIndexHTML("/addPatient");
+//for (int i = 0; i < 1000; i++)
+//    mapToIndexHTML("/builder;patientId=" + i);
 
 
 //Start server
