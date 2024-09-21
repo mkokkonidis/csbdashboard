ca9dea3c02d54504459e83c3f390805045556b69
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2024-09-22 01:50:49 +0300

    Paging for RPi data

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Http.Extensions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
+using static System.Net.WebRequestMethods;
+using System.Text;
+using System.Diagnostics.Metrics;
 
 namespace CSBDashboardServer.Controllers
 {
@@ -93,65 +96,87 @@ namespace CSBDashboardServer.Controllers
                 };
         }
 
-        static dynamic NonFHIRObservations(string auth, string apiBaseUrlSlash, string endPath, object? data = null)
+        static async Task<dynamic> NonFHIRObservations(string auth, string apiBaseUrlSlash, string endPath, object? data = null)
         {
-            const int pageSize = 100;
-
+            const int limit = 3000;
             var infoList = new List<string>();
+            var list = new List<object>();
 
-            string url = $"{apiBaseUrlSlash}nonfhir/api/{endPath}";
-            if (Verbose) infoList.Add($"Info: Results from {url} ");
             try
             {
                 int page = 0;
-                //while (true)
+                while (true)
                 {
 
-                    int count = 0;
-
-                    using (WebClient client = new WebClient())
+                    var handler = new HttpClientHandler
                     {
-                        client.Headers.Add("Accept", "application/json");
-                        client.Headers.Add("Content-Type", "application/json");
-                        client.Headers.Add("Authorization", auth);
-                        var jsonResponseBody = data == null?
-                            client.DownloadString(url):
-                            client.UploadString(url, JsonSerializer.Serialize(data));
+                        AllowAutoRedirect = true
+                    };
+
+                    //var client = new HttpClient(handler);
+
+                    // Force IPv4
+                    System.Net.ServicePointManager.Expect100Continue = false;
+                    System.Net.ServicePointManager.UseNagleAlgorithm = false;
+                    System.Net.ServicePointManager.EnableDnsRoundRobin = true;
+                    System.Net.ServicePointManager.DnsRefreshTimeout = 0; // Disa
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        string url = apiBaseUrlSlash+$"nonfhir/api/{endPath}?limit={limit}&offset={page}&ascending=false";
+                        if (Verbose) infoList.Add($"Info: Results from {url} ");
+
+                        client.DefaultRequestHeaders.Add("Accept", "application/json");
+                        //client.Default.Add("Content-Type", "application/json");
+                        client.DefaultRequestHeaders.Add("Authorization", auth);
+                        string jsonResponseBody;
+                        if (data == null)
+                        {
+                            jsonResponseBody = await client.GetStringAsync(url);
+                        }
+                        else { 
+                            var response = await client.PostAsync(url, new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json"));
+                            response.EnsureSuccessStatusCode();
+                            jsonResponseBody = await response.Content.ReadAsStringAsync();
+                        }
+
+
+
                         var responseBody = (System.Text.Json.JsonElement)
                             JsonSerializer.Deserialize<dynamic>(jsonResponseBody);
 
-                        return new
-                        {
-                            results = responseBody,
-                            debug = infoList
-                        };
-                        //if (Verbose) infoList.Add($"Info: deserialised");
-                        //var list = responseBody.GetProperty("entry").EnumerateArray();
-                        //if (Verbose) infoList.Add($"Info: entry array found");
-                        //foreach (var item in list)
-                        //{
-                        //    var resource = item.GetProperty("resource");
-                        //    var effectiveDateTime = resource.GetProperty("effectiveDateTime").GetString();
-                        //    var value =
-                        //        componentCode == null ?
-                        //            resource.GetProperty("valueQuantity").GetProperty("value").GetDecimal() :
-                        //            resource.GetProperty("component")
-                        //            .EnumerateArray().ToList()
-                        //                    .Where(_ =>
-                        //                           _.GetProperty("code")
-                        //                            .GetProperty("coding")[0]
-                        //                            .GetProperty("code").GetString() == componentCode)
-                        //                    .FirstOrDefault()
-                        //                    .GetProperty("valueQuantity").GetProperty("value").GetDecimal();
-                        //    retList.Add(new decimal[] {
-                        //        (Convert.ToDateTime(effectiveDateTime).Ticks - epochTicks)/10000,
-                        //        value });
-                        //    count++;
-                        //}
+                        var measurementEntries = responseBody.GetProperty("measurementEntries").EnumerateArray();
+                        int count = 0;
+                        foreach (var entry in measurementEntries) {
+                            /*{
+          "createdDateTime": "2024-09-20T20:00:06.821604",
+          "deviceId": "rpi",
+          "deviceImei": "e4:5f:01:e1:8b:08",
+          "measurementDatetime": "2024-09-20T20:00:06",
+          "measurementId": 14595,
+          "metadata": null,
+          "modifiedDateTime": "2024-09-20T20:00:06.821601",
+          "state": "1",
+          "type": "pollutionIndex",
+          "userId": "P72"
+        },*/
+                            list.Add(entry);
+                            count++;
+                        }
+                        if (count < limit) break;
+
+                        page++;
+
+
                     }
 
-                    //if (count < pageSize) break;
                 }
+                return new
+                {
+                    results = new {  measurementEntries = list } ,
+                    debug = infoList
+                };
+
+
             }
             catch (Exception ex)
             {
@@ -187,9 +212,18 @@ namespace CSBDashboardServer.Controllers
             var auth = Request.Headers.Authorization.ToString();
             var apiBaseUrlSlash = Environment.GetEnvironmentVariable("MAIN_URL"); // e.g., https://test-retention.biomed.ntua.gr/api/
 
+
+            if(id == 0)
+            {
+                id =72;
+                apiBaseUrlSlash = "https://staging-retention.biomed.ntua.gr/api/";
+                auth = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3eUtxLWdIQ3RFLTF2T1d2Q2otMFVzb3ZnaGY5UG4tOGp1NXZ2SWx1S3JRIn0.eyJleHAiOjE3MjY5NTg3OTQsImlhdCI6MTcyNjk1ODQ5NCwiYXV0aF90aW1lIjoxNzI2OTU3NDQ1LCJqdGkiOiIyNmM0ZThlYy0wMWQ2LTRiZGUtYjgzZi1iZjZiNTMzZDU4NzciLCJpc3MiOiJodHRwczovL3N0YWdpbmctcmV0ZW50aW9uLmJpb21lZC5udHVhLmdyL2F1dGgvcmVhbG1zL3JldGVudGlvbiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI1ODJiNzUzNi1mMGY0LTRkY2UtYjcxNC1jYTZkNzdiYmExN2MiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJkYXNoYm9hcmQiLCJub25jZSI6IjNmOWM1YmZiLWJiOWMtNGNmYy04ZTEzLTUwNTMyZTdmZWZjMyIsInNlc3Npb25fc3RhdGUiOiI5NThhN2JjYS03ODFlLTQ3M2EtODY0Mi1mYjkwMzI4ZDI0MTYiLCJhY3IiOiIwIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vc3RhZ2luZy1yZXRlbnRpb24uYmlvbWVkLm50dWEuZ3IiLCJodHRwOi8vbG9jYWxob3N0OjQyMDAiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImNsaW5pY2FsX2Nhc2VfbWFuYWdlciIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iLCJkZWZhdWx0LXJvbGVzLXJldGVudGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIG9yZ2FuaXNhdGlvbiBlbWFpbCBwcm9maWxlIiwic2lkIjoiOTU4YTdiY2EtNzgxZS00NzNhLTg2NDItZmI5MDMyOGQyNDE2IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJEZXYgQWNjb3VudCIsIm9yZ2FuaXNhdGlvbiI6IjE3IiwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGV2IiwiZ2l2ZW5fbmFtZSI6IkRldiIsImZhbWlseV9uYW1lIjoiQWNjb3VudCIsImVtYWlsIjoiZGV2QHRlc3QuY29tIn0.XvBZUSdXLAjvT5uFfcsqdfwL-tgHvunBWOpxkLqH_ar3If86429sI_R8XQirI7--GPRtR6scNX2d_-mO2_XuTmbhfUaG9HbCgO4MTjALzBSHRAhPcMtbWo2E1WhADqBl1AiWxsVl1msjhFFN00308QYEVzwUzueUNBiDK0SmMiYZSkd35_PToxuHvnwUp4p1EtuMuoECMGN5gHaMRxe8rC-SKxlWAplcYJbtBG7cgMB3FuKXANg_VD714Sq9pbOt9Cl3qNwGh8nrAxdfngcbRpa1rcR2lAUmzjseASP_Kw1SY6SgL_tb4Zi_U6dK5rRYU1w3zz_zFbY-C-_P3CXzaw";
+            }
+
             Func<string, Task<object>> O = async (spec) => await Task.Run(() => CompactFHIRObservations(auth, apiBaseUrlSlash, id, spec));
-            Func<string, Task<object>> GNO = async (path) => await Task.Run(() => NonFHIRObservations(auth, apiBaseUrlSlash, path));
-            Func<string, object, Task<object>> PNO = async (path, data) => await Task.Run(() => NonFHIRObservations(auth, apiBaseUrlSlash, path, data));
+            //Func<string, Task<object>> GNO = async (path) => await Task.Run(() => NonFHIRObservations(auth, apiBaseUrlSlash, path));
+            //Func<string, object, Task<object>> PNO = async (path, data) => await Task.Run(() => NonFHIRObservations(auth, apiBaseUrlSlash, path, data));
+            Func<string, object, Task<object>> PNO =  (path, data) => NonFHIRObservations(auth, apiBaseUrlSlash, path, data);
 
             var lightSleepTask = O("code=762636008&category=29373008");
             var deepSleepTask = O("code=762636008&category=60984000");
@@ -209,7 +243,7 @@ namespace CSBDashboardServer.Controllers
             var weightTask = O("code=726527001&category=408746007");
             var bloodPressureDiastolicTask = O("code=75367002&category=408746007&component-code=271649006");
             var bloodPressureSystolicTask = O("code=75367002&category=408746007&component-code=271650006");
-            var rasberryTask = PNO("Measurement/get?limit=3000&offset=0", new { userId = $"P{id}" });
+            var rasberryTask = PNO("Measurement/get", new { userId = $"P{id}" });
 
             await Task.WhenAll(lightSleepTask, deepSleepTask, remSleepTask, awakeningsTask, caloriesTask, metresAscendedTask, distanceTask, stepsTask, oxygenTask, bodyTemperatureTask, heartRateWatchMinTask, heartRateWatchAvgTask, heartRateWatchMaxTask, heartRateBloodPressureMeterTask, heartRateOximeterTask, weightTask, bloodPressureDiastolicTask, bloodPressureSystolicTask, rasberryTask);
 
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,11 +32,58 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+try
+{
+
+//    Ping ping = new Ping();
+//    PingReply reply = ping.Send("8.8.8.8");  // Google DNS server (IPv4)
+
+//    if (reply.Status == IPStatus.Success)
+//    {
+//        Console.WriteLine("Ping successful. Network is up.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ping failed. Check network connectivity.");
+//    }
+
+
+//    NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+//    foreach (NetworkInterface adapter in networkInterfaces)
+//    {
+//        // Get the IP properties of the network interface
+//        IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+
+//        // Get the DNS addresses associated with the network interface
+//        IPAddressCollection dnsServers = adapterProperties.DnsAddresses;
+
+//        if (dnsServers.Count > 0)
+//        {
+//            Console.WriteLine($"Network Interface: {adapter.Name}");
+//            Console.WriteLine("DNS Servers:");
+//            foreach (IPAddress dns in dnsServers)
+//            {
+//                Console.WriteLine($" - {dns}");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+//    var addresses = Dns.GetHostAddresses("google.com"); // Replace with your domain
+//    foreach (var address in addresses)
+//    {
+//        Console.WriteLine($"IP Address: {address}");
+//    }
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine($"DNS Resolution Error: {e.Message}");
+//}
+
 // Middleware to serve index.html for all non-file requests
 app.Use(async (context, next) =>
 {
     var path = context.Request.Path.Value;
-
     // Check if the requested path does not match an existing file
     if (!path.StartsWith("/values")
     && !File.Exists(Path.Combine(app.Environment.WebRootPath, path.TrimStart('/'))))
