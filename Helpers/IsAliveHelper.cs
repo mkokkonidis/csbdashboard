c3efdbf4f51e0bf83f6ea224a9064527907d210c
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2025-05-23 01:55:18 +0300

    4.0.19

---
+++ Helpers/IsAliveHelper.cs
@@ -0,0 +1,34 @@
+﻿using CSBDashboardServer.Controllers;
+using System.Net;
+using System.Text.Json;
+
+namespace CSBDashboardServer.Helpers
+{
+    public static class IsAliveHelper
+    {
+
+        public static dynamic IsAlive(string fhirBaseUrlDirect, string nonfhirBaseUrlDirect, string patientManagerBaseUrlDirect)
+        {
+
+            string url = $"{fhirBaseUrlDirect}/metadata";
+            
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("Accept", "application/fhir+json");
+                client.Headers.Add("Content-Type", "application/json");
+                var jsonResponseBody =
+                    client.DownloadData(url);
+                var responseBody = (System.Text.Json.JsonElement)
+                    JsonSerializer.Deserialize<dynamic>(jsonResponseBody);
+            }
+
+
+
+            return "OK";                
+ 
+    
+    }
+
+    }
+}
+
