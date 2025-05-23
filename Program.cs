fa456211e1cf82d2cee56fa59bffdb1cc80a91b9
Miltiadis Kokkonidis <m.kokkonidis@gmail.com>
2025-05-23 13:34:02 +0300

    20d

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var verbose = (Environment.GetEnvironmentVariable("VERBOSE") ?? "no").ToLower() == "yes";
+var verbose = true || (Environment.GetEnvironmentVariable("VERBOSE") ?? "no").ToLower() == "yes";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,7 +36,7 @@ app.UseAuthorization();
 // Middleware to serve index.html for all non-file requests
 app.Use(async (context, next) =>
 {
-    var path = context.Request.Path.Value;
+    var path = context.Request.Path.Value.ToLower();
     // Check if the requested path does not match an existing file
     if (!path.StartsWith("/fhirvalues") && !path.StartsWith("/nonfhirvalues") && !path.StartsWith("/isalive") 
     && !File.Exists(Path.Combine(app.Environment.WebRootPath, path.TrimStart('/'))))
